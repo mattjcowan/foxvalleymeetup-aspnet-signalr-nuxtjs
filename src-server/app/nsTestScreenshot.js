a8f6dae8562f3192ea4c12eaee449f0a8a494fd2
MJC <mjc@mjczone.com>
2019-03-17 22:49:05 -0500

    removed xtra files

--- src-server/app/nsTestScreenshot.js
+++ src-server/app/nsTestScreenshot.js
@@ -1,7 +1,7 @@
 var takeScreenshot = require('./nsTakeScreenshot');
 
 var url = 'https://devblogs.microsoft.com/dotnet/announcing-net-core-3-preview-3/'
-var imageName = './wwwroot/uploads/af25a89442674dfab717607cf45591bc-0.png';
+var imageName = './App_Data/uploads/af25a89442674dfab717607cf45591bc-0.png';
 
 var callback = function (err, file) {
     if (err) console.log('err', err);
